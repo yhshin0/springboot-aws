107d16d2fc2cb8a35f837ee438b28c6c5e6d12f4
yhshin0 <asyh1047@naver.com>
2021-09-03 22:54:05 +0900

    feat: 게시글 수정 구현

--- src/main/java/org/example/springboot/web/IndexController.java
+++ src/main/java/org/example/springboot/web/IndexController.java
@@ -1,11 +1,13 @@
 package org.example.springboot.web;
 
 import org.example.springboot.service.posts.PostsService;
+import org.example.springboot.web.dto.PostsResponseDto;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,5 +23,12 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
     }
 }
